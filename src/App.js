04b6a255721f314b5ebe114873c66f3ef2a17ae6
jpca999 <jpca999@gmail.com>
2019-12-17 18:50:35 -0500

    -

--- src/App.js
+++ src/App.js
@@ -9,30 +9,12 @@ import Main from './pages/Main/Index.js';
 
 import Search from './pages/Search/Index.js';
 
-import PageTwo from './pages/PageTwo'; 
-
-import HowItWorks from './pages/HowItWorks/Index.js';
-
-import ContactUs from './pages/ContactUs/Index.js';
-
-import WhyUs from './pages/WhyUs/Index.js';
-import Testimonials from './pages/Testimonials/Index.js'
-
-// import 'react-select/dist/react-select.css';
-
 
 const App = () =>{
     return (
         <BrowserRouter>
             <Route path="/" exact component={Main} />
             <Route path="/Search" exact component={Search} />
-            <Route path="/page2" exact component={PageTwo} />
-            <Route path="/Howitworks" exact component={HowItWorks} />
-            <Route path="/ContactUs" exact component={ContactUs} />            
-            
-            <Route path="/WhyUs" exact component={WhyUs} />
-            <Route path="/Testimonials" exact component={Testimonials} />
-
         </BrowserRouter>
         );
 }; 

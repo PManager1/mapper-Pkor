d73b9eb6598507634f70931b15a357eeba839686
jpca999 <jpca999@gmail.com>
2019-12-17 18:30:07 -0500

    -

--- src/pages/Search/Index.js
+++ src/pages/Search/Index.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import Playground from './Playground.js'; 
-import Grouped from './Grouped.js'; 
 import CheckboxLabels from './CheckboxLabels.js';
-
 import InputAdornments from './InputAdornments.js';
 
 const Search = ()=>{
     return (
         <div > 
-        <Playground />
-
-        <Grouped />
-        <InputAdornments />
-
+            <Playground />
+            <InputAdornments />
         </div>
     ); 
 }

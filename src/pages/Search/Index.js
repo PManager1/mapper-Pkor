1a40677ddda9f39c5f46ad5ba86121f4c6107051
jpca999 <jpca999@gmail.com>
2019-12-18 17:57:40 -0500

    -

--- src/pages/Search/Index.js
+++ src/pages/Search/Index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import SearchView from './SearchView.js'; 
-import CheckboxLabels from './CheckboxLabels.js';
-import SampleInputAdornments from '../../components/SampleInputAdornments.js';
+
 
 const Search = ()=>{
     return (
         <div > 
             <SearchView />
-            <CheckboxLabels />
-            <SampleInputAdornments />
         </div>
     ); 
 }

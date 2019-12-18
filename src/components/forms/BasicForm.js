405c8cae04dd1c13ed5cd41630f45e04a4396bb0
jpca999 <jpca999@gmail.com>
2019-12-18 11:51:22 -0500

    basic form now have 100% width layout

--- src/components/forms/BasicForm.js
+++ src/components/forms/BasicForm.js
@@ -24,8 +24,6 @@ export default function BasicForm() {
 
   return (
     <div className={classes.root}>
-      <Grid container spacing={3}>
-        
 
       <Formik
         initialValues={{
@@ -35,65 +33,22 @@ export default function BasicForm() {
     }}
   onSubmit={data => (alert(JSON.stringify(data)))}
   render={() => (
-    <Form mode='themed'>
-      
-      <Input
-        required={true}
-        name='FirstName'
-        label='First Name'
-        hint='This is a FirstName'
-      />
-      <Input
-        required={true}
-        name='LastName'
-        label='LastName'
-        hint='This is a LastName'
-      />
 
-      <PhoneInput
-        name='phoneNr5'
-        label='Phone Nr.'
-        format='NATIONAL'
-        defaultCountry='us'
-        placeholder='(555) 555-5555'
-        hint='This is a hint'
-      />
+<Grid container alignItems="center" spacing={3}>
 
-     <Input
-        required={true}     
-        name='Email'
-        label='Email'
-        hint='This is a Email'
-      />
+    <Form mode='themed' style={{ width: '100%'}}>
 
-    <Input  
-        required={true}    
-        name='Street Address'
-        label='StreetAddress'
-        hint='This is a Street Address'
-      />
-
-    <Input  
-        required={true}    
-        name='City'
-        label='City'
-        hint='This is a City'
-      />
-
-
-    <Select
-        name='State'
-        label='Select State'
-        placeholder='CA'
-        options={[
-          { value: '1', label: 'AK' },
-          { value: '2', label: 'AZ' },
-          { value: '3', label: 'CA' },
-          { value: '4', label: 'DC' },
-          { value: '5', label: 'LA' },
-          { value: '6', label: 'MD' },
-        ]}
-      />
+    <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            <Input
+              required={true}
+              name='FirstName'
+              label='First Name'
+              hint='This is a FirstName'
+            />
+        </Paper>
+       </Grid>
+ 
 
     <Input  
         required={true}    
@@ -107,11 +62,13 @@ export default function BasicForm() {
           </button>
           </div> 
     </Form>
+    </Grid>
+
   )}
 />
 
 
-      </Grid>
+      
     </div>
   );
 }

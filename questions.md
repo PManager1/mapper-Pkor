abd885d8fd56487b285e6ae9df3dad291a9e5d84
jpca999 <jpca999@gmail.com>
2019-12-17 21:49:35 -0500

    -

--- questions.md
+++ questions.md
@@ -4,6 +4,7 @@ Should we get the dump of all the objects or seperate it by indivudual calls.
 ===============
 employee type : would be of option select type or user can input text for it ?
 ===============
+sprint 1 end when ?
 
 ===============
 
--- src/App.js
+++ src/App.js
@@ -3,14 +3,10 @@ import { BrowserRouter, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Main from './pages/Main/Index.js';
-
 import FieldDetails from './pages/Main/FieldDetails.js';
-
-
 import AlignmentRadioBtns  from './components/Inputs/AlignmentRadioBtns.js';
 
 import Search from './pages/Search/Index.js';
-
 
 const App = () =>{
     return (

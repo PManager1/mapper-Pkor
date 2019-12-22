d2ae41abff9664a9d5b6a532975bfed359ee5514
jpca999 <jpca999@gmail.com>
2019-12-21 19:41:34 -0500

    -

---
+++ src/pages/Search/SelectedClientDetail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SelectedClientDetail = ()=>{
+    return (
+        <div > 
+            SelectedClientDetail
+        </div>
+    ); 
+}
+
+export default SelectedClientDetail; 

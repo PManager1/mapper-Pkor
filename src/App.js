7c5e974b8f4750814aaf427f808aa25f6db9d170
jpca999 <jpca999@gmail.com>
2019-12-22 19:11:47 -0500

    path not found working great now

--- src/App.js
+++ src/App.js
@@ -26,26 +26,25 @@ import { selectClient } from './actions';
 const App = () =>{
     return (
         <HashRouter>
-        <Switch>
+            <Switch>
+                <Route path="/ModalFormDialog" exact component={ModalFormDialog} />
+                <Route path="/drawer" exact component={TemporaryDrawer} />
+                <Route path="/login" exact component={Login} />
 
-        <Route path="/ModalFormDialog" exact component={ModalFormDialog} />
-        <Route path="/drawer" exact component={TemporaryDrawer} />
-        <Route path="/login" exact component={Login} />
-
-        <MainWrapper >
-            <Route path="/" exact component={ExpansionScreen} />
-            <Route path="/clientdetail" exact component={ClientDetail} />
-            
-            <Route exact path="/search" component={Search} />
-            <Route exact path="/search/:id" component={ClientDetail} />
-            
-            <Route path="/newField" component={NewFields} />
-            <Route path="/AllRules" component={AllRules} />
-            <Route path="/CreateNewRule" component={CreateNewRule} />
-            <Route path="/CreateFromExistingTemplate" component={CreateFromExistingTemplate} />
-            <Route path="/RecentlyCreatedTemplates" component={RecentlyCreatedTemplates} />
-            </MainWrapper>
-            <Route component={NotFound} />
+                <MainWrapper >
+                    <Route path="/" exact component={ExpansionScreen} />
+                    <Route path="/clientdetail" exact component={ClientDetail} />
+                    
+                    <Route exact path="/search" component={Search} />
+                    <Route exact path="/search/:id" component={ClientDetail} />
+                    
+                    <Route path="/newField" component={NewFields} />
+                    <Route path="/AllRules" component={AllRules} />
+                    <Route path="/CreateNewRule" component={CreateNewRule} />
+                    <Route path="/CreateFromExistingTemplate" component={CreateFromExistingTemplate} />
+                    <Route path="/RecentlyCreatedTemplates" component={RecentlyCreatedTemplates} />
+                    <Route component={NotFound} />
+                </MainWrapper>
             </Switch>
         </HashRouter>
         );

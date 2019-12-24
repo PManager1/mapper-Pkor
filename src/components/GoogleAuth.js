99ad1b8b8a202d2cca891abb9256315fb0649f7a
jpca999 <jpca999@gmail.com>
2019-12-24 11:39:26 -0500

    in signed in working on header -6

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,11 +1,36 @@
 import React from 'react'; 
 
+
 class GoogleAuth extends React.Component {
 
+    state = { isSignedIn: null }; 
+
+
+    componentDidMount(){
+        window.gapi.load('client:auth2', () =>{
+            window.gapi.client.init({
+                clientId: '652821201113-v1fh4132uukcmsmvdfjirvm1akrkf7k7.apps.googleusercontent.com',
+                scope: 'email'
+            }).then(() =>{
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState ({ isSignedIn: this.auth.isSignedIn.get() }) 
+            }); 
+        }); 
+    }
+
+renderAuthButton(){
+    if (this.state.isSignedIn === null ){
+        return <div> I dont know if we're signed in </div>
+    } else if( this.state.isSignedIn ){
+        return <div> Im signed in </div>
+    } else {
+        return  <div> Im not signed in </div>
+    }
+}
 
 render(){
     return(
-        <div> Google Auth</div>
+        <div> {this.renderAuthButton()} </div>
     ); 
 }
 }

dcde3999fe5887f8ca0df22fef9cf5df4d86b524
jpca999 <jpca999@gmail.com>
2019-12-27 17:14:33 -0500

    good styling for the items

--- src/pages/Later/Index.js
+++ src/pages/Later/Index.js
@@ -1,34 +1,53 @@
+// https://github.com/clauderic/react-sortable-hoc
 import React, {Component} from 'react';
 import {render} from 'react-dom';
-import {SortableContainer, SortableElement} from 'react-sortable-hoc';
+import {
+  sortableContainer,
+  sortableElement,
+  sortableHandle,
+} from 'react-sortable-hoc';
 import arrayMove from 'array-move';
 
-const SortableItem = SortableElement(({value}) => (
-  <li tabIndex={0}>{value}</li>
+import styles from './Item.css';
+
+
+const DragHandle = sortableHandle(() => <span>::</span>);
+
+const SortableItem = sortableElement(({value}) => (
+  <div className="theItem">
+    <DragHandle />
+    {value}
+  </div>
 ));
 
-const SortableList = SortableContainer(({items}) => {
-  return (
-    <ul>
-      {items.map((value, index) => (
-        <SortableItem key={`item-${value}`} index={index} value={value} />
-      ))}
-    </ul>
-  );
+const SortableContainer = sortableContainer(({children}) => {
+  return <ul>{children}</ul>;
 });
 
 export default class Later extends Component {
   state = {
     items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6'],
   };
+
   onSortEnd = ({oldIndex, newIndex}) => {
     this.setState(({items}) => ({
       items: arrayMove(items, oldIndex, newIndex),
     }));
   };
+
+  pressDelay = () =>{
+
+  }
+
   render() {
-    return <SortableList items={this.state.items} onSortEnd={this.onSortEnd} />;
+    const {items} = this.state;
+
+    return (
+      <SortableContainer onSortEnd={this.onSortEnd} pressDelay useDragHandle>
+        {items.map((value, index) => (
+          <SortableItem key={`item-${value}`} index={index} value={value} />
+        ))}
+      </SortableContainer>
+    );
   }
 }
-
-// render(<SortableComponent />, document.getElementById('root'));

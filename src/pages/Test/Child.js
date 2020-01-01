945a14cfd7b15bb5db12e0fdf477628c0b110aa0
jpca999 <jpca999@gmail.com>
2019-12-31 20:35:38 -0500

    plus button to add new field coming up right next to the header name

--- src/pages/Test/Child.js
+++ src/pages/Test/Child.js
@@ -3,11 +3,14 @@ import { connect } from "react-redux";
 import Typography from '@material-ui/core/Typography';
 import SortableComponent from './SortableComponent';
 
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import Tooltip from '@material-ui/core/Tooltip';
+import { Link } from 'react-router-dom';
 
 const Child = (props) => {
 
   console.log( '7 - this.props  in child component ', props.data  ); 
-
   useEffect(() => {
     // code to run on component mount
     // props.fetchClients(); 
@@ -17,8 +20,16 @@ const Child = (props) => {
     return (
       <div>
 
-
             <Typography variant='h5' align='left'> {props.data.RecordName}  </Typography>
+
+            <Typography paragraph align="right">
+              <Tooltip title="Add New Field" aria-label="add"> 
+                <Fab color="secondary" aria-label="add"   component={Link} to="/newfield" >
+                  <AddIcon /> 
+                  </Fab>
+                </Tooltip>
+            </Typography>  
+
             {/* we pass RecordId which has multiple fields in it. Now this one fetches & creates mutiple childs */}
             <SortableComponent Record={props.data}/>              
       </div>

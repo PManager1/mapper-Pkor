a9dc172874a5db4a22d560b3c3614a1ab27c7861
jpca999 <jpca999@gmail.com>
2020-01-01 17:11:50 -0500

    now the css of the Records Component looks good

--- src/pages/Test/RecordComponent.js
+++ src/pages/Test/RecordComponent.js
@@ -5,6 +5,7 @@ import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import RecordsForm from './RecordsForm/RecordsForm.js';
+
 import { makeStyles } from '@material-ui/core/styles';
 
 
@@ -20,26 +21,24 @@ const useStyles = makeStyles(theme => ({
     eachRow: {
         // backgroundColor: 'red',
         marginBottom: '1px', 
-        padding: '0 24px 0 24px',
+        // padding: '0 24px 0 24px',
       },
   }));
 
 
+
+
 const RecordComponent = ({value, DragHandle})=>{
-
-
-      const classes = useStyles();
-    
-    console.log(' 12  - RecordComponent - value = ', value ); 
-    console.log(' 13  - DragHandle = ', DragHandle ); 
+        const classes = useStyles();  
+    // console.log(' 12  - RecordComponent - value = ', value ); 
     return (
         <div className="root" >
-                <ExpansionPanel  className={classes.eachRow}>
+
+                <ExpansionPanel className={classes.eachRow}>
                     <ExpansionPanelSummary
                         expandIcon={<ExpandMoreIcon />}
                         aria-controls="panel1a-content"
                         id="panel1a-header"
-                        className={classes.eachRow}
                         >
                     <Typography className="heading"> <DragHandle /> {value.FieldName}</Typography>
 

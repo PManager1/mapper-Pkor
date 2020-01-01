b36c0b6f4539584c26400d68d2451d536fd4c91e
jpca999 <jpca999@gmail.com>
2020-01-01 00:19:33 -0500

    create logic page coming out to be good

--- src/pages/CreateNewLogic/Index.js
+++ src/pages/CreateNewLogic/Index.js
@@ -4,61 +4,82 @@ import { makeStyles } from '@material-ui/core/styles';
 import Button from "@material-ui/core/Button";
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormLabel from '@material-ui/core/FormLabel';
-
 import NewLogic from './NewLogic.js'; 
+
+import { Formik } from 'formik';
+import { Form, Input, PhoneInput, Select, SubmitBtn } from 'react-formik-ui';
+
+
+
+// const useStyles = makeStyles(theme => ({
+//   root: {
+//     flexGrow: 1,
+//   },
+//   textArea: {
+//     width: 900, 
+//     minHeight: 200,
+//   },
+// }));
+
+const textArea = {
+  width: '900', 
+  minHeight: '200',
+};
 
 
 const useStyles = makeStyles(theme => ({
-    root: {
-      flexGrow: 1,
-    },
-    textArea: {
-      width: 900, 
-      minHeight: 200,
-    },
-  }));
+  root: {
+    flexGrow: 1,
+  },
+  textArea: {
+    width: 900, 
+    minHeight: 200,
+  },
+}));
+  
+  // const classes = useStyles();
+
+  export default function CreateNewRule() {
+    const classes = useStyles();  
 
 
-const CreateNewRule = ()=>{
-    const classes = useStyles();
-
-
-    const handleSaveClick = () =>{
-        console.log('28 -  called handleSaveClick inside form.js ' ); 
-      }
-     
-
-    return (
-        <div className={classes.root}>
-
-           <TextareaAutosize
-           className={classes.textArea}
-            fullWidth
-            aria-label="maximum height"
-            placeholder="Please input your logic here"
-            defaultValue=""
+      return (
+      <Formik
+        initialValues={{
+          textFieldExample3: '',
+          LogicName: '', 
+          LastName: '', 
+        }}
+        onSubmit={  data => (alert(JSON.stringify(data)))  }
+        render={() => (
+          <Form mode='themed'>
+            
+            <Input
+              required='true'
+              name='LogicName'
+              label='Logic Name'
+              hint='This is a LogicName'
             />
 
+            <TextareaAutosize
+                      style={textArea}
+                      className={classes.textArea}
+                      fullWidth
+                      aria-label="maximum height"
+                      placeholder="Please input your logic here"
+                      defaultValue=""
+            />
+          
 
-    <Button  type="submit"
-        fullWidth
-        variant="contained" 
-        onClick = {handleSaveClick}
-        color="primary"
-      >
-        Save
-      </Button>
-
-      <Button
-        fullWidth
-        disableElevation
-        variant="contained"
-        color="default"
-      >
-        Cancel
-      </Button>
-        </div>
-    ); 
-}
-
-export default CreateNewRule; 
+            <div className="text-center">
+            <button type="submit" className="btn"  >
+                  submit button
+                </button>
+                </div> 
+          </Form>
+        )}
+      />
+      ); 
+};
+      
+// export default CreateNewRule; 

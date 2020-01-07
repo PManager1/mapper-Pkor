1767f7b1b26a8a789c186305dcdc8976ab1310d7
jpca999 <jpca999@gmail.com>
2020-01-06 20:12:53 -0700

    improed free solo

--- src/pages/Records/FreeSolo.js
+++ src/pages/Records/FreeSolo.js
@@ -20,14 +20,13 @@ export default function Tags() {
   return (
     <div className={classes.root}>
 
-      <p> third </p>
       <Autocomplete
       fullWidth
         multiple
         id="tags-filled"
         options={top100Films.map(option => option.title)}
         defaultValue={[top100Films[13].title]}
-        freeSolo
+        metaData
         renderTags={(value, getTagProps) =>
           value.map((option, index) => (
             <Chip variant="outlined" label={option} {...getTagProps({ index })} />
@@ -38,7 +37,7 @@ export default function Tags() {
           fullWidth
             {...params}
             variant="outlined"
-            label="freeSolo"
+            label="metaData"
             placeholder="Favorites"
             fullWidth
           />
@@ -50,9 +49,9 @@ export default function Tags() {
 
 // Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
 const top100Films = [
-  { title: 'The Shawshank Redemption', year: 1994 },
-  { title: 'The Godfather', year: 1972 },
-  { title: 'The Godfather: Part II', year: 1974 },
+  { title: 'Credit', year: 1994 },
+  { title: 'Debit', year: 1972 },
+  { title: 'Average', year: 1974 },
   { title: 'The Dark Knight', year: 2008 },
   { title: '12 Angry Men', year: 1957 },
   { title: "Schindler's List", year: 1993 },

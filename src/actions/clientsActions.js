dfb4c4fd112bab9bbe632eeb141c45997fd3d764
jpca999 <jpca999@gmail.com>
2020-01-06 20:19:22 -0700

    put the clients actions in seprat file k;

---
+++ src/actions/clientsActions.js
@@ -0,0 +1,47 @@
+import clients from '../apis/clients';
+
+export const createClient =  (formValues) => async dispatch => {
+    const response = await clients.post('/fieldlist', formValues ); 
+ 
+    dispatch ({  type: 'CREATE_CLIENT',   payload: response.data });
+    };
+
+
+export const editClient =  (formValues, clientID) => async dispatch => {
+    // console.log('4 --- inside  action createClient  formValues = ', formValues); 
+    const response = await clients.put(`/clients/${clientID}`, formValues); 
+
+    dispatch ({  type: 'EDIT_CLIENT',   payload: response.data });
+    };
+
+
+export const deleteClient =  (clientID) => async dispatch => {
+    const response = await clients.delete(`/clients/${clientID}`); 
+
+    dispatch ({  type: 'DELETE_CLIENT',   payload: clientID });
+    };
+    
+
+
+
+export const fetchClients =  () =>{
+    return async dispatch => {
+    const response = await clients.get('/clients'); 
+    // console.log('7 ---  action fetchClients  response = ', response.data ); 
+
+    dispatch ({  type: 'FETCH_CLIENTS',   payload: response.data });
+    }
+};
+
+
+export const fetchSingleClient = (clientID) =>{
+console.log('15 ---  action fetchSingleClient  clientID = ', clientID ); 
+return async dispatch => {
+    const response = await clients.get(`/clients/${clientID}`); 
+                                                 
+    // console.log('19 ---  action fetchClients  response = ', response.data ); 
+
+    dispatch ({  type: 'FETCH_SINGLE_CLIENT',   payload: response.data });
+    }
+};
+
--- src/actions/index.js
+++ src/actions/index.js
@@ -5,6 +5,8 @@ export * from './recordsActions';
 export * from './authActions';
 export * from './fieldsActions';
 export * from './logicActions';
+export * from './clientsActions'; 
+
 
 // import { showLoading, hideLoading } from 'actions/index.js';
 
@@ -22,49 +24,4 @@ export const hideLoading = () =>{
 
 
 
-
-export const createClient =  (formValues) => async dispatch => {
-    const response = await clients.post('/fieldlist', formValues ); 
- 
-    dispatch ({  type: 'CREATE_CLIENT',   payload: response.data });
-    };
-
-
-export const editClient =  (formValues, clientID) => async dispatch => {
-    // console.log('4 --- inside  action createClient  formValues = ', formValues); 
-    const response = await clients.put(`/clients/${clientID}`, formValues); 
-
-    dispatch ({  type: 'EDIT_CLIENT',   payload: response.data });
-    };
-
-
-export const deleteClient =  (clientID) => async dispatch => {
-    const response = await clients.delete(`/clients/${clientID}`); 
-
-    dispatch ({  type: 'DELETE_CLIENT',   payload: clientID });
-    };
-    
-
-
-
-export const fetchClients =  () =>{
-    return async dispatch => {
-    const response = await clients.get('/clients'); 
-    // console.log('7 ---  action fetchClients  response = ', response.data ); 
-
-    dispatch ({  type: 'FETCH_CLIENTS',   payload: response.data });
-    }
-};
-
-
-export const fetchSingleClient = (clientID) =>{
-console.log('15 ---  action fetchSingleClient  clientID = ', clientID ); 
-return async dispatch => {
-    const response = await clients.get(`/clients/${clientID}`); 
-                                                 
-    // console.log('19 ---  action fetchClients  response = ', response.data ); 
-
-    dispatch ({  type: 'FETCH_SINGLE_CLIENT',   payload: response.data });
-    }
-};
 

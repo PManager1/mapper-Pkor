4d200ccf4401fe7438c22847220d0673a7bd8e05
jpca999 <jpca999@gmail.com>
2020-01-07 18:50:19 -0800

    moveing to /records/280394820984023n on selecging the id

--- src/App.js
+++ src/App.js
@@ -73,11 +73,15 @@ const App = () =>{
                 <Route exact path="/rulepicker" exact component={RulePicker} />
                 <Route exact path="/later" exact component={Later} />
                     {/* <Route exact path="/" exact component={ExpansionScreen} /> */}
-                    <Route exact path="/" exact component={Records} />
                     
                     <Route exact path="/clientdetail" exact component={ClientDetail} />
                     <Route exact path="/search" component={Search} />
                     <Route exact path="/search/:id" component={ClientDetail} />
+
+                    <Route exact path="/records" exact component={Records} />
+                    <Route exact path="/records/:id" component={Records} />
+                    
+                    
                     <Route exact path="/newField" component={NewFields} />
                     <Route exact path="/newrecord" component={NewRecord} />
                     <Route exact path="/editrecords" component={EditRecords} />

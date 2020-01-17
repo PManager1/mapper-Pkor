f199bf5c800b61a743f06a02241c1953fe94d349
Jay <jpca999@gmail.com>
2020-01-16 21:20:55 -0800

    Updates

---
+++ precache-manifest.2f8985cea39c48280b836be91a547816.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84a5ca421e841f3cfaa2a327a156204d",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "03a40f794e3d78a72f9c",
+    "url": "/my-app/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "1248c17e0e72d7c4ebd9",
+    "url": "/my-app/static/css/main.3915d1e7.chunk.css"
+  },
+  {
+    "revision": "03a40f794e3d78a72f9c",
+    "url": "/my-app/static/js/2.38c09be7.chunk.js"
+  },
+  {
+    "revision": "811077a41351833202762aca0f83c778",
+    "url": "/my-app/static/js/2.38c09be7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1248c17e0e72d7c4ebd9",
+    "url": "/my-app/static/js/main.47b15f6a.chunk.js"
+  },
+  {
+    "revision": "d1d5d8510331bd48e1b6",
+    "url": "/my-app/static/js/runtime-main.89795f5b.js"
+  }
+]);

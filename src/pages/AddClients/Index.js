c94d2b3650f95d427566262dc929c053050ae3d1
Jay <jpca999@gmail.com>
2020-01-24 09:30:45 -0800

    -

--- src/pages/AddClients/Index.js
+++ src/pages/AddClients/Index.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
 import AddClientsList from './AddClientsList';
-
 
 const AllRules = ()=>{
     return (
         <div >
+            <Typography variant='h4' color="primary" align="left">
+                    Add Clients to current Map
+                </Typography>
+
+
             <AddClientsList />
         </div>
     );

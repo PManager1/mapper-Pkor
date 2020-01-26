d77e2428492996901fd9ca854cf055d5460211b0
Jay <jpca999@gmail.com>
2020-01-25 21:34:13 -0800

    removin gthe tex.tjs  file

--- src/pages/Search/test.js
+++ src/pages/Search/test.js
@@ -1,20 +1,20 @@
-import React from "react";
-import { connect } from "react-redux";
-import { addCount } from "./store/counter/actions";
+// import React from "react";
+// import { connect } from "react-redux";
+// import { addCount } from "./store/counter/actions";
 
-export const Count = ({ count, addCount }) => {
-  return (
-    <main>
-      <div>Count: {count}</div>
-      <button onClick={addCount}>Add to count</button>
-    </main>
-  );
-};
+// export const Count = ({ count, addCount }) => {
+//   return (
+//     <main>
+//       <div>Count: {count}</div>
+//       <button onClick={addCount}>Add to count</button>
+//     </main>
+//   );
+// };
 
-const mapStateToProps = state => ({
-  count: state.counter.count
-});
+// const mapStateToProps = state => ({
+//   count: state.counter.count
+// });
 
-const mapDispatchToProps = { addCount };
+// const mapDispatchToProps = { addCount };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Count);
+// export default connect(mapStateToProps, mapDispatchToProps)(Count);
